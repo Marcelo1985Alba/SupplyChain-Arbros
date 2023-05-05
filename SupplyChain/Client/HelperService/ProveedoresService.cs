b6fadb6f82047078d32e05933caa4adbb13acb4a
Marcelo Albarracin <elchelamiente_25@hotmail.com>
2023-05-05 19:19:17 -0300

    05052023- correccion en proveedores

--- SupplyChain/Client/HelperService/ProveedoresService.cs
+++ SupplyChain/Client/HelperService/ProveedoresService.cs
@@ -1,34 +1,29 @@
-﻿//using Microsoft.AspNetCore.Mvc;
-//using Microsoft.JSInterop;
-//using SupplyChain.Client.HelperService.Base;
-//using SupplyChain.Client.RepositoryHttp;
-//using SupplyChain.Shared;
-//using SupplyChain.Shared.Enum;
-//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Net.Http;
-//using System.Threading.Tasks;
-//using System.Timers;
+﻿using Microsoft.JSInterop;
+using SupplyChain.Client.HelperService.Base;
+using SupplyChain.Client.RepositoryHttp;
+using SupplyChain.Shared;
+using SupplyChain.Shared.Enum;
+using SupplyChain.Shared.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Timers;
 
-//namespace SupplyChain.Client.HelperService
-//{
-//    public class ProveedoresService : BaseService<vProveedorItris, int>
-//    {
-//        private readonly IRepositoryHttp _httpClient;
-//        private readonly HttpClient Http;
-//        private const string api = "API/Prove";
-//        public ProveedoresService(IRepositoryHttp httpClient, string api) : base(httpClient, api)
-//        {
-//            _httpClient = httpClient;
-            
-//        }
-//        [HttpGet("Get")]
-//        public async Task<HttpResponseWrapper<List<vProveedorItris>>> Get()
-//        {
-//            return await http.GetFromJsonAsync<List<vProveedorItris>>($"api/Proveedores/Get");
-//        }
-        
-        
-//    }  
-//}
+namespace SupplyChain.Client.HelperService
+{
+    public class ProveedoresService : BaseService<vProveedorItris, int>
+    {
+        private const string API = "api/Proveedores";
+
+        public ProveedoresService(IRepositoryHttp httpClient): base(httpClient, API)
+        {
+        }
+
+        public async Task<HttpResponseWrapper<List<vProveedorItris>>> GetProveedoresItris()
+        {
+            var response = await base.http.GetFromJsonAsync<List<vProveedorItris>>($"{API}/GetProveedoresItris");
+            return response;
+        }
+    }
+}
