b3f8983891944010cc16579a859a11229600895b
thiagoAE <sistemas@aerre.com.ar>
2023-07-19 08:15:24 -0300

    19072023 Commit para actualizar produccion

---
+++ SupplyChain/Client/HelperService/ComprasService.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.JSInterop;
+using SupplyChain.Client.HelperService.Base;
+using SupplyChain.Client.RepositoryHttp;
+using SupplyChain.Shared.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SupplyChain.Client.HelperService
+{
+    public class ComprasService : BaseService<Compra, int>
+    {
+        private const string API = "api/Compra";
+        private readonly IJSRuntime js;
+
+        public ComprasService(IJSRuntime js, IRepositoryHttp httpClient) : base(httpClient, API)
+        {
+            this.js = js;
+        }
+
+        public async Task<HttpResponseWrapper<Compra>> Anular(int item)
+        {
+            return await http.GetFromJsonAsync<Compra>($"{API}/Anular/{item}");
+        }
+    }
+}
