1baf5d7eee606c1b3e151e3fc31baf4805910d46
MatiasMutz <108437589+MatiasMutz@users.noreply.github.com>
2024-01-22 12:23:10 -0300

    Update service-worker.published.js

--- SupplyChain/Client/wwwroot/service-worker.published.js
+++ SupplyChain/Client/wwwroot/service-worker.published.js
@@ -2,7 +2,7 @@
 // Caution! Be sure you understand the caveats before publishing an application with
 // offline support. See https://aka.ms/blazor-offline-considerations
 
-var cacheNameVersion = 'scPWA-v0.80.43';
+var cacheNameVersion = 'scPWA-v0.81.00';
 
 self.importScripts('./service-worker-assets.js');
 self.addEventListener('install', event => event.waitUntil(onInstall(event)));
